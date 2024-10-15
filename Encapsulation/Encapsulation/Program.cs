a18bd705f33256cb48e04d232b5768f0cd630500
huseyn-dev <huseyn.developer.15@gmail.com>
2024-10-15 11:18:22 +0400

    encapsulation task

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Encapsulation;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.Name = "Eltac";
+        person.Age = 5;
+        person.Birthday = new DateTime(2005, 12, 25);
+        Console.WriteLine($"Sizin adiniz {person.Name}-dir");
+        Console.WriteLine($"Sizin yashiniz { person.Age}-dir");
+        Console.WriteLine(person.Birthday);
+
+
+    }
+}
