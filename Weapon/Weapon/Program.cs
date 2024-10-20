51584dba197a592d60c428daafc0bea8938281bc
huseyn-dev <huseyn.developer.15@gmail.com>
2024-10-20 20:33:17 +0400

    weapon project

---
+++ Weapon/Weapon/Program.cs
@@ -0,0 +1,109 @@
+﻿namespace Weapon;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int capacityMag;
+        while (true)
+        {
+        Console.WriteLine("Enter the capacity of the magazine");
+        capacityMag =int.Parse(Console.ReadLine());
+            if (capacityMag>0)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("The capacity of the magazine cannot be negative");
+            }
+        }
+
+        int bulletinMag;
+        while (true)
+        {
+        Console.WriteLine("Enter the number of bullets in the magazine:");
+        bulletinMag = int.Parse(Console.ReadLine());
+            if (bulletinMag<=capacityMag && bulletinMag>0)
+            {
+                break;
+            }
+            else if(bulletinMag > capacityMag)
+            {
+                Console.WriteLine("The number of bullets cannot exceed the capacity of the magazine, re-enter.");
+            }
+            else
+            {
+                Console.WriteLine("The number of bullets cannot be negative");
+            }
+
+        }
+
+
+        string fireMode;
+        while (true)
+        {
+        Console.WriteLine("Enter fire mode: (single/automatic)");
+        fireMode = Console.ReadLine();
+            if (fireMode=="single" || fireMode=="automatic")
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Please enter the correct information: (single/automatic) ");
+            }
+
+        }
+
+
+
+
+        Console.WriteLine("Choose an item between 0-6");
+
+        string res = "0 - İnformasiya almaq üçün\r\n1 - Shoot metodu üçün\r\n2 - Fire metodu üçün\r\n3 - GetRemainBulletCount metodu üçün\r\n4 - Reload metodu üçün\r\n5 - ChangeFireMode metodu üçün\r\n6 - Proqramdan dayandırmaq üçün\r\nqısayoldur.";
+        Console.WriteLine(res);
+        Weapon weapon = new Weapon( capacityMag, bulletinMag, fireMode);
+        bool running = true;
+        while (running)
+        {
+            string num = Console.ReadLine();
+            switch (num) { 
+
+                case "0":
+
+                    Console.WriteLine($"Magazine's maximum bullet capacity {weapon.capacityMag}");
+                    Console.WriteLine($"The number of bullets in the magazine: {weapon.bulletinMag} ");
+                    Console.WriteLine($"Atish modu {weapon.fireMode}");
+
+                    break;
+                case "1":
+                    weapon.Shoot();
+                    break;
+                case "2":
+                    weapon.Fire();
+                    break;
+                case "3":
+                    weapon.GetRemainBulletCount();
+                    break;
+                case "4":
+                    weapon.Reload();
+                    break;
+                case "5":
+                    weapon.ChangeFireMode();
+                    break;
+                case "6":
+                    running = false;
+                    Console.WriteLine("The program has been stopped");
+                    break;
+                default:
+                    Console.WriteLine("Choose an item between 0-6");
+                    break;
+
+
+            }
+        }
+
+    }
+}
+
---
+++ Weapon/Weapon/Weapon.cs
@@ -0,0 +1,86 @@
+﻿namespace Weapon;
+
+public class Weapon
+{
+    public int capacityMag { get; set; }
+    public int bulletinMag {
+        get;
+        set;
+    }
+    public string fireMode { get; set; }    
+    
+    
+
+    public Weapon(int capacityMag, int bulletinMag, string fireMode)
+    {
+        this.capacityMag = capacityMag;
+        this.bulletinMag = bulletinMag;
+        this.fireMode = fireMode;
+    }
+
+    public void Shoot()
+    {
+        if (bulletinMag > 0)
+        {
+
+            bulletinMag-=1;
+            Console.WriteLine($"Fired, {bulletinMag} bullets left ");
+        }
+        else
+        {
+            Console.WriteLine("The magazine is empty.");
+        }
+
+    }
+    public void Fire()
+    {
+        if (fireMode=="single")
+        {
+            Shoot();
+            Console.WriteLine("Since it's in single mode, it fires one shot at a time. Switch the mode to fire automatic.");
+        }
+        else
+        {
+            if (bulletinMag > 0)
+            {
+                DateTime strart = DateTime.Now;
+                while (bulletinMag > 0)
+                {
+                    Shoot();
+                }
+                DateTime end = DateTime.Now;
+                TimeSpan duration = end - strart;
+                Console.WriteLine($"All the bullets in the magazine have been fired : {duration}");
+            }
+            else
+            {
+                Console.WriteLine("The magazine is empty.");
+            }
+        }
+    }
+    public void GetRemainBulletCount()
+    {
+        int needBullet = capacityMag - bulletinMag;
+        Console.WriteLine($"{needBullet} bullets are needed to fully load the magazine");
+
+    }
+    public void Reload()
+    {
+        bulletinMag = capacityMag;
+        Console.WriteLine($"The magazine has been reloaded, the number of bullets in the magazine:{bulletinMag} ");
+    }
+    public void ChangeFireMode()
+    {
+        if (fireMode == "single")
+        {
+            fireMode = "automatic";
+        }
+        else
+        {
+            fireMode = "single";
+        }
+        Console.WriteLine($"The firing mode has been changed, fire mode:{fireMode}");
+    }
+
+}
+
