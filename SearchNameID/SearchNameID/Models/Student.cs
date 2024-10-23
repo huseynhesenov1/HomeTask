8006a8cd2ffa7c25a6671c5847c86e406ee167f6
huseyn-dev <huseyn.developer.15@gmail.com>
2024-10-23 14:07:05 +0400

    a

---
+++ SearchNameID/SearchNameID/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchNameID.Models
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string email { get; set; }
+        public string PhoneNumber { get; set; }
+        public double GPA { get; set; }
+        public string Status{ get; set; }
+        public string Major {  get; set; }
+    }
+}
