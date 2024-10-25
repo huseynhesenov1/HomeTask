1bc42b11ffd18101628114c16fcd34a8a441df83
huseyn-dev <huseyn.developer.15@gmail.com>
2024-10-25 04:45:35 +0400

    custom indexer

---
+++ CustomIndexer/CustomIndexer/Models/StringList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomIndexer.Models;
+
+public class StringList
+{
+    City[] cities;
+    public StringList()
+    {
+        cities = new City[0];
+    }
+    public void AddCity(City city)
+    {
+        Array.Resize(ref cities, cities.Length + 1);
+        cities[cities.Length - 1] = city;
+        Console.WriteLine("City Added to Cities array");
+    }
+    public City[] AddCities(params City[] inputCity)
+    {
+        Array.Resize(ref cities, cities.Length + inputCity.Length);
+        for (int i = 0; i < inputCity.Length; i++)
+        {
+            cities[cities.Length - inputCity.Length + i] = inputCity[i];
+
+        }
+        return cities;
+    }
+    public int this[string cityName]
+    {
+        get
+        {
+            for (int i = 0; i < cities.Length; i++)
+            {
+                if (cities[i].Name == cityName)
+                {
+                    return i; 
+                }
+            }
+            return -1; 
+        }
+    }
+}
---
+++ CustomIndexer/CustomIndexer/Program.cs
@@ -0,0 +1,26 @@
+﻿using CustomIndexer.Models;
+
+namespace CustomIndexer;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        StringList objCity = new StringList();
+        City city1 = new City(){ Name = "Jalilabad"};
+        City city2 = new City(){ Name = "Lerik"};
+        City city3 = new City(){ Name = "Baku"};
+        City city4 = new City(){ Name = "Ganga"};
+        objCity.AddCities(city1,city2,city3,city4);
+        City city5 = new City() { Name = "Lankaran" };
+        City[] cities = objCity.AddCities(city5);
+
+
+
+
+        // indexer usuli ile axtarmaq 
+        string searchCity = Console.ReadLine();
+        int index = objCity[searchCity];
+        Console.WriteLine(index);
+    }
+}
