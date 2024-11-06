7b6cb66f32159b7beff87ddedeaa181ce574b51c
huseyn-dev <huseyn.developer.15@gmail.com>
2024-11-06 05:51:48 +0400

    directory task

---
+++ DirectoryTask2/DirectoryTask2/Program.cs
@@ -0,0 +1,53 @@
+﻿//using System.Text.Json.Serialization;
+//using Newtonsoft.Json;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace DirectoryTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\ACER\\source\\repos\\DirectoryTask2\\DirectoryTask2";
+            
+            Directory.CreateDirectory(path+ "\\Models");
+            Directory.CreateDirectory(path+ "\\Data");
+
+           
+            if (!File.Exists(path + "\\Data" + "\\jsonData.json"))
+            {
+                File.Create(path + "\\Data" + "\\jsonData.json");
+            }
+
+            
+            List<string> urls = new List<string>()
+            {
+                "https://jsonplaceholder.typicode.com/posts",
+
+            };
+
+
+            Console.WriteLine("sinxron method ile");
+            Retrieved(urls);
+            
+        }
+        public static void Retrieved(List<string> urls)
+        {
+            string path = "C:\\Users\\ACER\\source\\repos\\DirectoryTask2\\DirectoryTask2";
+
+            HttpClient client = new HttpClient();
+            foreach (string url in urls)
+            {
+                //Console.WriteLine(client.GetStringAsync(url).Result.ToString());
+                //string serialized = JsonConvert.SerializeObject(client.GetStringAsync(url).Result);
+                //Console.WriteLine(serialized);
+                using (StreamWriter sw = new StreamWriter(path + "\\Data" + "\\jsonData.json"))
+                {
+                    sw.WriteLine(client.GetStringAsync(url).Result);
+                    sw.Close();
+                }
+            }
+        }
+    }
+}
