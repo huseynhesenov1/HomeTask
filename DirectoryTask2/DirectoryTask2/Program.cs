7a4dd6a63278269716ef4fcf54bfcf47c1916587
huseyn-dev <huseyn.developer.15@gmail.com>
2024-11-06 10:33:24 +0400

    async method and directory

--- DirectoryTask2/DirectoryTask2/Program.cs
+++ DirectoryTask2/DirectoryTask2/Program.cs
@@ -1,6 +1,6 @@
-﻿//using System.Text.Json.Serialization;
-//using Newtonsoft.Json;
+﻿
 using Newtonsoft.Json;
+using System.Diagnostics;
 using System.IO;
 
 namespace DirectoryTask2
@@ -9,45 +9,98 @@ namespace DirectoryTask2
     {
         static void Main(string[] args)
         {
-            string path = "C:\\Users\\ACER\\source\\repos\\DirectoryTask2\\DirectoryTask2";
-            
-            Directory.CreateDirectory(path+ "\\Models");
-            Directory.CreateDirectory(path+ "\\Data");
+            #region Task2
+            //string path = "C:\\Users\\ACER\\source\\repos\\DirectoryTask2\\DirectoryTask2";
 
-           
-            if (!File.Exists(path + "\\Data" + "\\jsonData.json"))
-            {
-                File.Create(path + "\\Data" + "\\jsonData.json");
-            }
+            //Directory.CreateDirectory(path+ "\\Models");
+            //Directory.CreateDirectory(path+ "\\Data");
 
-            
+
+            //if (!File.Exists(path + "\\Data" + "\\jsonData.json"))
+            //{
+            //    File.Create(path + "\\Data" + "\\jsonData.json");
+            //}
+
+
+            //List<string> urls = new List<string>()
+            //{
+            //    "https://jsonplaceholder.typicode.com/posts",
+
+            //};
+            //  RetrievedSerilaize(urls);
+
+
+            #endregion
+
+
+            #region Task1
+
+          /*  Stopwatch sw = new Stopwatch();
+            sw.Start();
+
             List<string> urls = new List<string>()
             {
-                "https://jsonplaceholder.typicode.com/posts",
-
+            "https://www.blackbox.ai/",
+            "https://profile.w3schools.com/log-in?redirect_url=https%3A%2F%2Fwww.w3schools.com%2F",
+            "https://www.w3schools.com/cs/index.php",
+            "https://learn.microsoft.com/en-us/dotnet/csharp/",
+            "https://learn.microsoft.com/en-us/aspnet/core/?view=aspnetcore-8.0",
+            "https://dotnet.microsoft.com/en-us/download",
             };
 
 
-            Console.WriteLine("sinxron method ile");
-            Retrieved(urls);
-            
+
+
+            //Console.WriteLine("Sinxron method ile");
+            //Retrieved(urls);
+            //Console.WriteLine(sw.ElapsedMilliseconds);
+
+
+
+
+
+            Console.WriteLine("Asinxron method ile");
+            RetrievedAsync(urls);
+            Console.WriteLine(sw.ElapsedMilliseconds);
+          */
+
+            #endregion
         }
-        public static void Retrieved(List<string> urls)
+        public static void RetrievedSerilaize(List<string> urls)
         {
             string path = "C:\\Users\\ACER\\source\\repos\\DirectoryTask2\\DirectoryTask2";
 
             HttpClient client = new HttpClient();
             foreach (string url in urls)
             {
-                //Console.WriteLine(client.GetStringAsync(url).Result.ToString());
-                //string serialized = JsonConvert.SerializeObject(client.GetStringAsync(url).Result);
-                //Console.WriteLine(serialized);
+                string jsonData = client.GetStringAsync(url).Result;
+
+                string serialized = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(jsonData), Formatting.Indented);
+                
                 using (StreamWriter sw = new StreamWriter(path + "\\Data" + "\\jsonData.json"))
                 {
-                    sw.WriteLine(client.GetStringAsync(url).Result);
-                    sw.Close();
+                    sw.WriteLine(serialized);
                 }
             }
+        }
+        public static void Retrieved(List<string> urls)
+        {
+            HttpClient client = new HttpClient();
+            foreach (string url in urls)
+            {
+                client.GetStringAsync(url).Result.ToString();
+            }
+        }
+
+        public static async Task RetrievedAsync(List<string> urls)
+        {
+            HttpClient httpClient = new HttpClient();
+            List<Task<string>> tasks = new List<Task<string>>();
+            foreach (string url in urls)
+            {
+                tasks.Add(httpClient.GetStringAsync(url));
+            }
+            await Task.WhenAll(tasks);
         }
     }
 }
--- OI_bound/ConsoleApp12/obj/Release/net8.0/ConsoleApp12.AssemblyInfo.cs
+++ OI_bound/ConsoleApp12/obj/Release/net8.0/ConsoleApp12.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp12")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b6cb66f32159b7beff87ddedeaa181ce574b51c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp12")]
