af249490aca013450b4353faff8e0b666f5dedf8
huseyn-dev <huseyn.developer.15@gmail.com>
2024-11-20 11:52:25 +0400

    added mvc task

---
+++ Entity Framework/task_mvc/task_mvc/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using task_mvc.Models;
+
+namespace task_mvc.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			Student student = new Student()
+			{
+				Id = 1,
+				Name="Huseyn",
+				Surname = "Hesenov",
+				Age=20
+			};
+			Student student1 = new Student()
+			{
+				Id = 1,
+				Name = "Hesen",
+				Surname = "Hesenov",
+				Age = 19
+			};
+			List<Student> students = new List<Student>()
+			{
+				student, student1
+			};
+			//JsonResult jsonResult = new JsonResult(student);
+			return View(students);
+		}
+
+	}
+}
---
+++ Entity Framework/task_mvc/task_mvc/Models/Groupp.cs
@@ -0,0 +1,10 @@
+﻿namespace task_mvc.Models
+{
+	public class Groupp
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int MemberCount { get; set; }
+			
+	}
+}
