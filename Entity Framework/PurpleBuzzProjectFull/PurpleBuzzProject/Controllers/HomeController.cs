4f08bf06df2773cbac4a9c72b84f6a14726a521e
huseyn-dev <huseyn.developer.15@gmail.com>
2024-11-21 11:56:26 +0400

    frort to back full ready

---
+++ Entity Framework/PurpleBuzzProjectFull/PurpleBuzzProject/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PurpleBuzzProject.Models;
+
+namespace PurpleBuzzProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Userr userr = new Userr()
+            {
+                Id = 1,
+                Name = "Huseyn"
+            };
+            Userr userr1 = new Userr()
+            {
+                Id = 2,
+                Name = "Hesen"
+            };
+            List<Userr> userrList = new List<Userr>() { userr,userr1 };
+                return View(userrList);
+        }
+    }
+}
