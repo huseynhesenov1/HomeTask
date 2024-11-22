03992b6ae860b96196b9c0f94e98f0768572811a
huseyn-dev <huseyn.developer.15@gmail.com>
2024-11-22 11:19:49 +0400

    database

---
+++ PurpleBuzzProjectFull/PurpleBuzzProject/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using PurpleBuzzProject.DAL;
+
+namespace PurpleBuzzProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<AppDbContext>(
+                options=>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("MsSql")));
+            var app = builder.Build();
+
+
+            app.UseStaticFiles();
+
+
+
+            app.MapControllerRoute(
+                name: default,
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+
+            app.Run();
+        }
+    }
+}
